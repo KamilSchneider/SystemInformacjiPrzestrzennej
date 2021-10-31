675be9193a0f5aa3f423132242e5d25960fc3fe9
KamilSchneider <93484579+KamilSchneider@users.noreply.github.com>
2021-10-31 18:42:43 +0100

    Add files via upload

---
+++ Podstrony/data/PrzystankiPKP_10.js
@@ -0,0 +1,8 @@
+var json_PrzystankiPKP_10 = {
+"type": "FeatureCollection",
+"name": "PrzystankiPKP_10",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nazwa:": "Olszyna Lubańska" }, "geometry": { "type": "Point", "coordinates": [ 15.3806451, 51.06969 ] } }
+]
+}
---
+++ Podstrony/data/PrzystankiPKP_11.js
@@ -0,0 +1,8 @@
+var json_PrzystankiPKP_11 = {
+"type": "FeatureCollection",
+"name": "PrzystankiPKP_11",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nazwa:": "Olszyna Lubańska" }, "geometry": { "type": "Point", "coordinates": [ 15.3806451, 51.06969 ] } }
+]
+}
